fcc2592767d39caf49c61586fa8a198951f6a194
chicoumbetov <shyngysmaaan@gmail.com>
2023-03-05 15:54:20 +0100

    First commit

---
+++ Messenger_AngimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Messenger_AngimeApp.swift
+//  Messenger Angime
+//
+//  Created by Shynggys Umbetov on 05/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Messenger_AngimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
