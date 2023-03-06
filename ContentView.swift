66a59444a8fee80e33170422eeaf36ae18ff4aa0
chicoumbetov <shyngysmaaan@gmail.com>
2023-03-06 21:28:47 +0100

    :lipstick: ContentView

--- ContentView.swift
+++ ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    var messageArray = ["Hello", "How are you doing ?", "I've been building SwiftUI app"]
+    
     var body: some View {
         VStack {
             TitleRow()
+            
+            ScrollView{
+                ForEach(messageArray, id: \.self) { text in
+                    MessageBubble(message: Message(id: "12345", text: text, received: true, timestamp: Date()))
+                }
+            }
+            .padding(.top, 10)
+            .background(.white)
         }
         .background(Color("Peach"))
     }
